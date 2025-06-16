4ff0daf1d9cf2e0d14ae7ad88855e004839acfd6
Suecia76 <carrascosanti76@gmail.com>
2025-06-16 11:55:05 -0300

    Primer commit

---
+++ AppCoder/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=100)
+    pais = models.CharField(max_length=50)
+    fundacion = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+    fecha_publicacion = models.DateField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.titulo
